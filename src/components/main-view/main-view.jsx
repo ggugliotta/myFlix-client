9e46021943f8484e60bbe06a479c23b5d94ec9a6
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-09-29 12:09:15 -0500

    log-in page and token

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,25 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localstorage.getItem("token");
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken: null);
 
     useEffect(() => {
-      fetch ("https://moviesapi-zy5e.onrender.com")
+      if (!token) return;
+
+      fetch ("https://moviesapi-zy5e.onrender.com", {
+        headers: { Authorization: 'Bearer ${token}' }
+      })
         .then((response) => response.json())
-        .then((data) => {
+        .then((movies) => {
+          console.log(movies);
           const moviesFromApi = data.docs.map((doc) => {
             return {
               id: doc.key,
@@ -21,16 +31,54 @@ export const MainView = () => {
 
           setMovies(moviesFromApi);
         });
-    }, []);
+    }, [token]);
+
+  if (!user) {
+    return (
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+    );
+  }
 
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+          >
+            Logout   
+          </button>
+          <MovieView
+           movie={selectedMovie} 
+           onBackClick={() => setSelectedMovie(null)} 
+           />
+        </>
     );
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null); 
+            localStorage.clear();        
+           }}
+        >
+          Logout  
+        </button>
+        <div>The list is empty!</div>
+      </>
+    );
   } 
 
 return (
