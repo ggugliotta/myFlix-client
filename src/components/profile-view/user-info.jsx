a1b5ca1994a2e7b1dd668e5c10fa57142d1f4e6a
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-06 12:36:27 -0500

    added more detail to user info card

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Proptypes from 'proptypes';
+import Card from 'react-bootstrap';
 
-function UserInfo({ email, name }) {
+export const UserCard = ({ user }) => {
     return (
-      <>
-        <h4>Your Information</h4>
-        <p>Name: {name}</p>
-        <p>E-mail: {user.Email}</p>
-      </>
-    )
-}
+      <Card classname="h-100">
+        <Card.Body>
+          <Card.Title>Your Information</Card.Title>
+            <Card.Text>
+              Name: {user.Name}
+              E-mail: {user.Email}
+              Birthday {user.Birthday}
+              Favorite Movies {user.favoriteMovies}
+            </Card.Text>
+        </Card.Body>
+      <Card/>
+    );
+  };
 
-export default UserInfo
+
+UserCard.propTypes = {
+  users: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthday: PropTypes.number.isRequired,
+    favoriteMovies: PropTypes.array.isRequired,
+  }).isRequired,
+};
