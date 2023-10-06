4f2e627fe9ec742ffbd0b82b867e91d994b98fee
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-06 12:41:52 -0500

    profile-view export changed

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,65 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Col, Row, Card } from 'react-bootstrap';
-import UserInfo from './user-info';
-import FavoriteMovies from './favorite-movies';
-import UpdateUser from './update-user';
-import './profile-view.scss';
+import {FavoriteMovies} from "../profile-view/favorite-movies.jsx";
+import {ProfileView} from "../profile-view/profile-view.scss";
+import {UserInfo} from "../profile-view/user-info.jsx";
+import {UpdateUser} from "../profile-view/update-user.jsx";
+import { Container, Row, Col} from "react-bootstrap";
 
-export const ProfileView ({ user, movies }) => {
-    // const [user] = useState({
-    //     Username: '',
-    //     Email: '',
-    //     FavoriteMovies: []
-    // });
-    // const favoriteMovies ({movies.filter}) => user.FavoriteMovies.includes(movie.id);
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    // };
-    // const handleRemove(movies) => {movies.list.filter((movie.id) => movie.id);
-    // const handleUpdate = (e) => {profile-view};
+export const ProfileView = () => {
+    const [users, setUsers] = useState([]);
+    const handleSubmit = (event) => {event.preventDefault();};
+    //const handleRemove = 
+    //const handleUpdate = () => {};
+    const favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id))
 
-    // useEffect(() => {
-    //           if (!token) return;
-    //   fetch ("https://moviesapi-zy5e.onrender.com/${favoriteMovies}", {
-    //     headers: { Authorization: `Bearer ${token}` }
-    //   })
-    //     .then((response) => response.json())
-    //     .then((movies) => {
-    //       const moviesFromApi = movies.map((movie) => {
-    //         return {
-    //           id: movie._id,
-    //           title: movie.Title,
-    //           imageURL: movie?.ImageUrl ?? movie?.ImagePath ?? '',
-    //         };
-    //       });
+    useEffect(() => {
+      if (!token) return;
+      fetch ("https://moviesapi-zy5e.onrender.com/users", {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then((response) => response.json())
+        .then((users) => {
+          const usersFromApi = users.map((user) => {
+            return {
+              _id: user._id,
+              name: user.Name,
+              username: user.Username,
+              password: user.Password,
+              email: user.Email,
+              birthday: user.Birthday,
+              favoriteMovies: user.favoriteMovies
+           };
+         });
 
-    //       setMovies(moviesFromApi);
-    //     });
-    // }, [token]);
+        setUsers(usersFromApi);
+        });
+    }, [token]);
 
     return (
-      <Container>
-        hello 
-        {/* /* { <Row>
-            <Col xs={12} sm={4}>
-                <Card>
-                    <Card.Body>
+        <Container>
+            <Row>
+                 <Col xs={12} sm={4}>
+                     <Card>
+                        <Card.Body>
                            <UserInfo name={user.Username} email={user.Email} />
-                    </Card.Body>
-                </Card>
+                        </Card.Body>
+                    </Card>
            
-            </Col>
-            <Col xs={12} xs={8}>
-                <Card>
-                    <Card.Body>
-                        <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-                    </Card.Body>
-                </Card>
-            </Col>
+                </Col>
+
+                 <Col xs={12} sm={8}>
+                     <Card>
+                        <Card.Body>
+                            <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+                        </Card.Body>
+                    </Card>
+
+                </Col>
         </Row>
        
-        <FavoriteMovies favoriteMovieList={favoriteMovieList} />/} */ */}
+        <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+        <Link to={`/`}>
+            <Button variant="primary">Home</Button>
+        </Link> 
       
       </Container>
-    );
+    )
 }
