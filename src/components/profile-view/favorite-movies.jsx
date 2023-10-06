e3e116fb542b58a49a09853bcdb83710f56bf5ec
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-05 19:42:53 -0500

    added Card

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,23 +1,45 @@
 import React from "react";
+import {  Row, Col, Figure, Button, Card } from "react-bootstrap";
+import "./profile-view.scss";
 
 function FavoriteMovies({ favoriteMovieList }) {
     return (
-        <div>
-            <h2>Favorite Movies</h2>
-            {favoriteMovieList.map((movie) => {
-                return (
-                    <div key={movies._id}>
-                        <img src={movies.ImageURL} />
-                        <Link to={`/movies/${movies._id}`}>
-                             <h4>{movies.Title}</h4>
-                         </Link>
-                         <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove from list</button>
-                    </div>
-                 )
-            })
-            }
-        </div>
+        <Card>
+            <Card.Body>
+                <Row>
+                    <Col xs={12}>
+                        <h4>Favorite Movies</h4>
+                 </Col>
+                 </Row>
+
+                <Row>
+                    {favoriteMovieList.map(({ ImageURL, Title, _id}) => {
+                         return (
+                            <Col xs={12} md={6} lg={4} key={_id} className="fav-movie">
+                                <Figure>
+                                <Link to={`/movies/${_id}`}>
+                                    <Figure.Image
+                                        src={ImageURL}
+                                        alt={Title}
+                                    />
+                                    <Figure.Caption>
+                                        {Title}
+                                    </Figure.Caption>
+                                    </Link>
+                                </Figure>
+                                <Link to={`/movies/${movies._id}`}>
+                                 <h4>{movies.Title}</h4>
+                                 </Link>
+                                 <Button variant="secondary" onClick={() => removeFav(movies._id)}>Remove</Button>
+                            </Col>
+                        )
+                     })
+                    }
+                </Row>
+            </Card.Body>
+        </Card>
     )
-}
+            
+}   
 
 export default FavoriteMovies 
