1a65d65988221ac032c660dc5aeef9b2907b4de8
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-06 12:39:46 -0500

    added movie.card.scss and button formatting

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import './movie-card.scss';
 
 //MovieCard Function Component
 export const MovieCard = ({ movie }) => {
     return (
-       <Card>
-        <Card.Img variant="top" src={movie.imageURL} />
+       <Card className="h-100">
+        <Card.Img variant="top" src={movie.imageURL} class="img-fluid" alt="Responsive image"/>
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.director.name} </Card.Text>
           <Link to={`/movies/${encodeURIComponent(movie.Id)}`}>
-            <Button variant="link">Open</Button>
+            <Button variant="primary">Open</Button>
           </Link> 
         </Card.Body>
        </Card>
@@ -34,3 +35,4 @@ MovieCard.propTypes = {
     rating: PropTypes.string.isRequired
   }).isRequired,
 };
+
