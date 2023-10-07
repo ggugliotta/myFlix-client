7a7d8538cd34da8eef763684cd7c7508b70b0d78
Jorge Cortés <jorge.cortes@gmail.com>
2023-10-06 17:26:25 -0700

    some improvements

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,45 +1,36 @@
 import React from "react";
-import {  Row, Col, Figure, Button, Card } from "react-bootstrap";
+import { Row, Col, Figure, Button, Card } from "react-bootstrap";
 import "./profile-view.scss";
 
-function FavoriteMovies({ favoriteMovieList }) {
-    return (
-        <Card>
-            <Card.Body>
-                <Row>
-                    <Col xs={12}>
-                        <h4>Favorite Movies</h4>
-                 </Col>
-                 </Row>
+export const FavoriteMovies = ({ favoriteMovieList }) => {
+  return (
+    <Card>
+        <Card.Body>
+            <Row>
+                <Col xs={12}>
+                    <h4>Favorite Movies</h4>
+                </Col>
+            </Row>
 
-                <Row>
-                    {favoriteMovieList.map(({ ImageURL, Title, _id}) => {
-                         return (
-                            <Col xs={12} md={6} lg={4} key={_id} className="fav-movie">
-                                <Figure>
+            <Row>
+                {favoriteMovieList.map(({ imageURL, title, id}) => {
+                    return (
+                        <Col xs={12} md={6} lg={4} key={_id} className="fav-movie">
+                            <Figure>
                                 <Link to={`/movies/${_id}`}>
-                                    <Figure.Image
-                                        src={ImageURL}
-                                        alt={Title}
-                                    />
-                                    <Figure.Caption>
-                                        {Title}
-                                    </Figure.Caption>
-                                    </Link>
-                                </Figure>
-                                <Link to={`/movies/${movies._id}`}>
-                                 <h4>{movies.Title}</h4>
-                                 </Link>
-                                 <Button variant="secondary" onClick={() => removeFav(movies._id)}>Remove</Button>
-                            </Col>
-                        )
-                     })
-                    }
-                </Row>
-            </Card.Body>
-        </Card>
-    )
-            
-}   
-
-export default FavoriteMovies 
+                                    <Figure.Image src={ImageURL} alt={Title} />
+                                    <Figure.Caption>{Title}</Figure.Caption>
+                                </Link>
+                            </Figure>
+                            <Link to={`/movies/${movies._id}`}>
+                                <h4>{movies.Title}</h4>
+                            </Link>
+                            <Button variant="secondary" onClick={() => removeFav(movies._id)}>Remove</Button>
+                        </Col>
+                    )
+                })}
+            </Row>
+        </Card.Body>
+    </Card>
+  );
+}
--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,67 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import {FavoriteMovies} from "../profile-view/favorite-movies.jsx";
+import {FavoriteMovies} from "./favorite-movies.jsx";
 import {ProfileView} from "../profile-view/profile-view.scss";
 import {UserInfo} from "../profile-view/user-info.jsx";
 import {UpdateUser} from "../profile-view/update-user.jsx";
-import { Container, Row, Col} from "react-bootstrap";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const ProfileView = () => {
-    const [users, setUsers] = useState([]);
+export const ProfileView = ({ user, movies, token, syncUser }) => {
+
     const handleSubmit = (event) => {event.preventDefault();};
-    //const handleRemove = 
+    //const handleRemove =
     //const handleUpdate = () => {};
-    const favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id))
+    const favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m.id))
 
-    useEffect(() => {
-      if (!token) return;
-      fetch ("https://moviesapi-zy5e.onrender.com/users", {
-        headers: { Authorization: `Bearer ${token}` }
-      })
-        .then((response) => response.json())
-        .then((users) => {
-          const usersFromApi = users.map((user) => {
-            return {
-              _id: user._id,
-              name: user.Name,
-              username: user.Username,
-              password: user.Password,
-              email: user.Email,
-              birthday: user.Birthday,
-              favoriteMovies: user.favoriteMovies
-           };
-         });
+    // useEffect(() => {
+    //   if (!token) return;
+    //   fetch ("https://moviesapi-zy5e.onrender.com/users", {
+    //     headers: { Authorization: `Bearer ${token}` }
+    //   })
+    //     .then((response) => response.json())
+    //     .then((users) => {
+    //       const usersFromApi = users.map((user) => {
+    //         return {
+    //           _id: user._id,
+    //           name: user.Name,
+    //           username: user.Username,
+    //           password: user.Password,
+    //           email: user.Email,
+    //           birthday: user.Birthday,
+    //           favoriteMovies: user.favoriteMovies
+    //        };
+    //      });
 
-        setUsers(usersFromApi);
-        });
-    }, [token]);
+    //     setUsers(usersFromApi);
+    //     });
+    // }, [token]);
 
-    return (
-        <Container>
-            <Row>
-                 <Col xs={12} sm={4}>
-                     <Card>
-                        <Card.Body>
-                           <UserInfo name={user.Username} email={user.Email} />
-                        </Card.Body>
-                    </Card>
-           
-                </Col>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <UserInfo user={user} />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              {/* <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} /> */}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
-                 <Col xs={12} sm={8}>
-                     <Card>
-                        <Card.Body>
-                            <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-                        </Card.Body>
-                    </Card>
+      <FavoriteMovies favoriteMovieList={favoriteMovies} />
 
-                </Col>
-        </Row>
-       
-        <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-        <Link to={`/`}>
-            <Button variant="primary">Home</Button>
-        </Link> 
-      
-      </Container>
-    )
+      <Link to={`/`}>
+        <Button variant="primary">Home</Button>
+      </Link>
+    </Container>
+  )
 }
