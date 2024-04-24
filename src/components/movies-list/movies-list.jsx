1580ebec8b7e1277b9cb9cf3564d7239db5e0f31
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2024-04-23 22:51:43 -0500

    commented out movies-list

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,36 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieFilter } from "../movie-filter/movie-filter";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { MovieCard } from "../movie-card/movie-card";
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
 
-export const MoviesList = () => {
-  let movies = useSelector((state) => state.movies.list);
-  const filter = useSelector((state) => state.movies.filter)
-    .trim()
-    .toLowerCase();
+// export const MoviesList = () => {
+//   let movies = useSelector((state) => state.movies.list);
+//   const filter = useSelector((state) => state.movies.filter)
+//     .trim()
+//     .toLowerCase();
 
-  const filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(filter)
-  );
+//   const filteredMovies = movies.filter((movie) =>
+//     movie.title.toLowerCase().includes(filter)
+//   );
 
-  return (
-    <>
-      <Row>
-        <MoviesFilter />
-      </Row>
-      <Row>
-        {movies.length === 0 ? (
-          <Col>The list is empty!</Col>
-        ) : (
-          filteredMovies.map((movie) => (
-            <Col className="mb-4" key={movie.id} md={3}>
-              <MovieCard movie={movie} />
-            </Col>
-          ))
-        )}
-      </Row>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Row>
+//         <MoviesFilter />
+//       </Row>
+//       <Row>
+//         {movies.length === 0 ? (
+//           <Col>The list is empty!</Col>
+//         ) : (
+//           filteredMovies.map((movie) => (
+//             <Col className="mb-4" key={movie.id} md={3}>
+//               <MovieCard movie={movie} />
+//             </Col>
+//           ))
+//         )}
+//       </Row>
+//     </>
+//   );
+// };
